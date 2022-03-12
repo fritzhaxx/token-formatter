c1fecfd7de2e57c2a5d3b732089ae990b8ecb194
FRITZ <83903458+fritzhaxx@users.noreply.github.com>
2022-03-12 09:21:00 -0800

    Update formatter.py
    Fixed weird issues

--- formatter.py
+++ formatter.py
@@ -1,26 +1,26 @@
 tokens = [x.strip() for x in open('tokens.txt', 'r', encoding="utf8").readlines()]
 output = open("output.txt", "a+")
-class funcs:
- def f1():
+
+def f1():
     for i in tokens:
         t = i.split(":")
         output.write(f"{t[1]}:{t[0]}:{t[2]}\n")
 
- def f2():
+def f2():
     for i in tokens:
         t = i.split(":")
         output.write(f"{t[1]}:{t[2]}:{t[0]}\n")
 
- def f3():
+def f3():
     for i in tokens:
         t = i.split(":")
         output.write(f"{t[1]}\n")
- def f4():
+def f4():
     for i in tokens:
         t = i.split(":")
         output.write(f"{t[0]}:{t[2]}:{t[1]}\n")
-fr = funcs()
-modes = {'1': fr.f1, '2': fr.f2, '3': fr.f3,'4':fr.f4}
+    
+modes = {'1': f1, '2': f2, '3': f3,'4':f4}
 
 print("""
 YOU NEED TO HAVE EMAIL:TOKEN:PASS TOKENS TO USE THAT FORMATTER
